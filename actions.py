0d1fd5abbae88e8bcf273d6c5d3a9f2d6878a507
A-Alhashem <q8_ash@live.com>
2018-11-23 06:49:31 +0300

    submission

--- actions.py
+++ actions.py
@@ -2,13 +2,15 @@
 from data import  population, clubs
 from components import Club, Person
 
+
 my_name = "Abdulaziz"
 my_age = 31
 my_bio = "Learning to Program"
 myself = Person(my_name, my_bio, my_age)
 
 def introduction():
-	print("Hello, %s. Welcome to our portal." % my_name)
+	print("\nHello, %s. Welcome to our portal." % my_name)
+	
 
 def options():
 	# your code goes here!
@@ -18,7 +20,7 @@ def options():
 	print("2) Browse and join clubs")
 	print("3) View existing club")
 	print("4) Display members of a club")
-	print("-1) Close application")
+	print("-1) Close application\n")
 	user_option = input(">")
 	return user_option
 
@@ -48,19 +50,25 @@ def create_club():
 	club.assign_president(myself)
 
 	# get the members to recuit to the club
-	print("Enter the numbers of the people you would like to recruit to your new club (-1 to stop): ")
+	print("\nEnter the numbers of the people you would like to recruit to your new club (-1 to stop): ")
 	print_population()
 	person_to_be_recruited = ""
-	while person_to_be_recruited != -1:
-		person_to_be_recruited = int(input("> "))
-		if person_to_be_recruited == "":
-			print("Please enter a valid number")
+	while person_to_be_recruited != "-1":
+		person_to_be_recruited = input("> ")
+		if person_to_be_recruited.isdigit() and int(person_to_be_recruited) <= len(population):
+			club.recruit_member(population[int(person_to_be_recruited)-1])
 		else:
-			club.recruit_member(population[person_to_be_recruited-1])
+			print("Please enter a valid number between 1 and 15\n")
+	#	if person_to_be_recruited == "" or person_to_be_recruited != person_to_be_recruited.isdigit():
+	#		print("Please enter a valid number")
+	#	elif int(person_to_be_recruited) > 15 or int(person_to_be_recruited) < -1:
+	#		print("That number is not available. Please pick a number between 1 to 15.\nOr type \"-1\" to stop")
+	#	else:
+	#		club.recruit_member(population[person_to_be_recruited-1])
 
 
 	# print the new club
-	print("Here's your new club...")
+	print("\nHere's your new club...")
 	print(club.name)
 	print(club.description)
 	club.print_member_list()
@@ -72,7 +80,7 @@ def create_club():
 def view_clubs():
 	# your code goes here!
 	for club in clubs:
-		print("NAME: %s\nDESCRIPTION: %s\nMEMBERS: %s\n" % (club.name, club.description, len(club.members)))
+		print("\nNAME: %s\nDESCRIPTION: %s\nMEMBERS: %s\n" % (club.name, club.description, len(club.members)))
 	
 
 def view_club_members():
@@ -80,7 +88,7 @@ def view_club_members():
 	view_clubs()
 	club_found = True
 	while club_found:
-		club_name = input("Enter the name of the club who's members you'd like to see: ")
+		club_name = input("\nEnter the name of the club who's members you'd like to see:\n ")
 		for club in clubs:
 			if club.name.lower() == club_name.lower():
 				club.print_member_list()
@@ -91,10 +99,10 @@ def join_clubs():
 	# your code goes here!
 	view_clubs()
 	club_name = input("Enter the name of the club you would like to join: \n")
-	for name in clubs:
-		if name.name == club_name:
-			name.recuit_member(myself)
-			print("%s has just joined %s!" % (myself.name, name.name))
+	for join in clubs:
+		if join.name.lower() == club_name.lower():
+			join.recruit_member(myself)
+			print("%s has just joined %s!" % (myself.name, join.name))
 
 	
 
